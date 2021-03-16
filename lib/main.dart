70145fd84f4f7d03209cd96e4967033dcf1127c0
lonfo fofe blondeau <blondeaufofe00@gmail.com>
2021-03-16 11:05:40 +0100

    use showcase

---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:showcaseview/showcaseview.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ShowCaseWidget(
+          builder: Builder(
+              builder: (_) => MyHomePage(title: 'Flutter Demo Home Page'))),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  final keyOne = GlobalKey();
+  final keyTwo = GlobalKey();
+  final key3 = GlobalKey();
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) =>
+        ShowCaseWidget.of(context).startShowCase([keyOne, keyTwo, key3]));
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: [
+          Showcase(
+            key: keyOne,
+            description: "test de menu",
+            child: IconButton(icon: Icon(Icons.favorite), onPressed: () {}),
+            contentPadding: EdgeInsets.all(12.0),
+          ),
+          Showcase(
+              key: keyTwo,
+              description: "descrioption two ",
+              child: IconButton(icon: Icon(Icons.settings), onPressed: () {})),
+        ],
+        leading: Builder(
+          builder: (BuildContext context) {
+            return Showcase(
+              description: "my menu",
+              key: key3,
+              child: IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
+                },
+                tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+              ),
+            );
+          },
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
